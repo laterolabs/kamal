a32f956087f1bb7e1fd30c1dc28878400b86f085
Derek Yau <derek@laterolabs.com>
2025-04-25 11:34:10 -0700

    Add base changes from https://github.com/basecamp/kamal/compare/main...kpumuk:kamal:custom-ssl so we can temporarily use this until kamal officially supports it

--- lib/kamal/configuration/validator/proxy.rb
+++ lib/kamal/configuration/validator/proxy.rb
@@ -3,12 +3,20 @@ class Kamal::Configuration::Validator::Proxy < Kamal::Configuration::Validator
     unless config.nil?
       super
 
-      if config["host"].blank? && config["hosts"].blank? && config["ssl"]
-        error "Must set a host to enable automatic SSL"
-      end
+      # if config["host"].blank? && config["hosts"].blank? && config["ssl"]
+      #   error "Must set a host to enable automatic SSL"
+      # end
 
       if (config.keys & [ "host", "hosts" ]).size > 1
         error "Specify one of 'host' or 'hosts', not both"
+      end
+
+      if config["ssl_certificate_path"].present? && config["ssl_private_key_path"].blank?
+        error "Must set a private key path to use a custom SSL certificate"
+      end
+
+      if config["ssl_private_key_path"].present? && config["ssl_certificate_path"].blank?
+        error "Must set a certificate path to use a custom SSL private key"
       end
     end
   end
